b20c2a1c2e2579f398245afab2a2087d0d2c26a8
mosespace <kiskayemoses@gmail.com>
2025-03-09 19:56:55 +0300

    feat: initial commit

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 export default function loading() {
   return (
     <div className="flex items-center justify-center mx-auto h-screen overflow-hidden max-w-full w-full">
